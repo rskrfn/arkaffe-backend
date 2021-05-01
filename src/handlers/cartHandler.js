c52183d4cc829a5a9fbb4cfc83ba3943f508b4c4
BudiSetyo <budisetyo176@gmail.com>
2021-05-01 15:27:09 +0700

    fix cart api

--- src/handlers/cartHandler.js
+++ src/handlers/cartHandler.js
@@ -11,38 +11,18 @@ const createCart = async (req, res) => {
       return;
     }
 
-    const cartTaken = (await cartModel.getCartUser(userId)) || [];
+    const createCart = await cartModel.addCart(userId);
 
-    if (cartTaken.length < 1) {
-      const createCart = await cartModel.addCart(userId);
+    if (createCart) {
+      const cartTaken = (await cartModel.getCartUser(userId)) || [];
 
-      const taken = (await cartModel.getCartUser(userId)) || [];
-      const cartId = taken[0].id;
+      const index = cartTaken.length - 1;
 
-      const createCartItem = await cartModel.addCartItem(
-        cartId,
-        productId,
-        quantity,
-        sizeId,
-      );
+      const cartId = cartTaken[index].id;
 
-      if (createCart && createCartItem) {
-        return responseStandard(res, 'product added to cart', {}, 200, true);
-      }
-    }
+      cartModel.addCartItem(cartId, productId, quantity, sizeId);
 
-    if (cartTaken.length > 0) {
-      const cartId = cartTaken[0].id;
-      const createCartItem = await cartModel.addCartItem(
-        cartId,
-        productId,
-        quantity,
-        sizeId,
-      );
-
-      if (createCartItem) {
-        return responseStandard(res, 'product added to cart', {}, 200, true);
-      }
+      responseStandard(res, 'product added to cart'), {}, 200, true;
     }
   } catch (err) {
     console.log(err);
--- src/routers/transactionRouter.js
+++ src/routers/transactionRouter.js
@@ -2,5 +2,6 @@ const Router = require('express').Router();
 const transactionHandler = require('../handlers/transactionHandler');
 
 Router.post('/:cartId', transactionHandler.addTransaction);
+Router.patch('/:cartId', transactionHandler.updateTransaction);
 
 module.exports = Router;
