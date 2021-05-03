7a701abe553d17de104d80b2a2a853280c635ce9
BudiSetyo <budisetyo176@gmail.com>
2021-05-03 16:02:39 +0700

    update mid authentication

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -1,24 +1,32 @@
 const responseStandard = require('../helpers/response');
 const jwt = require('jsonwebtoken');
 const { SECRET_KEY } = process.env;
+const authModel = require('../models/authModel')
 
 const authentikasi = (req, res, next) => {
-  const { authorization } = req.headers;
-  if (authorization && authorization.startsWith('Bearer ')) {
-    const token = authorization.slice(7, authorization.length);
-    try {
-      const verify = jwt.verify(token, SECRET_KEY);
-      if (verify) {
-        req.user = verify;
-        next();
-      } else {
-        return responseStandard(res, 'Unauthorized', {}, 401, false);
-      }
-    } catch (err) {
-      return responseStandard(res, err.message, {}, 500, false);
+  try {
+    const { authorization } = req.headers;
+    const token = authorization?.split(' ')[1];
+
+    if (!token) {
+      return responseStandard(res, 'No token provided', {}, 400, false);
     }
-  } else {
-    return responseStandard(res, 'Forbidden Access', {}, 403, false);
+
+    const verify = jwt.verify(token, SECRET_KEY);
+    if (!verify) {
+      return responseStandard(res, 'Unauthorized access', {}, 403, false);
+    }
+
+    const tokenTaken = await authModel.getToken(token);
+    if (tokenTaken.length > 0) {
+      return responseStandard(res, 'You have to login', {}, 400, false);
+    }
+
+    req.user = verify;
+    return next();
+  } catch (err) {
+    console.log(err);
+    return responseStandard(res, err, {}, 500, false);
   }
 };
 
